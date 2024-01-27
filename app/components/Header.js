007084d4d2ec0ec33e3113e5d5a4800e7a54e77f
Chong Xian <chongxian@easystore.co>
2024-01-27 23:46:34 +0800

    added multiple processes section

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="px-5 fixed top-5 w-full">
-      <header className="flex px-6 py-3 rounded-lg backdrop-blur-md w-full bg-white bg-opacity-30">
+    <div className="fixed top-0 w-full">
+      <header className="flex px-11 py-5 rounded-lg backdrop-blur-md w-full bg-white bg-opacity-10">
         <Image src="/images/logo.svg" alt="Logo" width={60} height={24} priority />
       </header>
     </div>
--- app/sections/Processes.js
+++ app/sections/Processes.js
@@ -1,22 +1,84 @@
-import Image from "next/image";
+"use client";
 
-export default async function Processes() {
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Processes() {
+  const [showingSection, setShowingSection] = useState("develop");
+
   return (
     <section className="mx-10 my-10 flex justify-between items-center h-screen">
       <div className="flex flex-col justify-center">
         <h1 className="text-xl">In my daily work, I do</h1>
         <div className="my-5">
-          <div className="text-6xl font-bold my-6 text-[#9747FF]">Design</div>
-          <div className="text-6xl font-bold my-6">Develop</div>
-          <div className="text-6xl font-bold my-6">Empower</div>
+          <div
+            className={`text-5xl font-bold my-6 ${showingSection == "design" ? "text-[#9747FF]" : "text-gray-200"}`}
+            onClick={() => setShowingSection("design")}
+          >
+            Design
+          </div>
+          <div
+            className={`text-5xl font-bold my-6 ${showingSection == "develop" ? "text-[#0085FF]" : "text-gray-200"}`}
+            onClick={() => setShowingSection("develop")}
+          >
+            Develop
+          </div>
+          <div
+            className={`text-5xl font-bold my-6 ${showingSection == "empower" ? "text-[#E19A0B]" : "text-gray-200"}`}
+            onClick={() => setShowingSection("empower")}
+          >
+            Empower
+          </div>
         </div>
         <div class="text-xl mt-10">Passionate on drawing clean UI design and provide easy to use experience</div>
         <div className="mt-5">
           <div className="px-5 py-1 rounded-full bg-gray-200 w-fit">Figma</div>
         </div>
       </div>
-      <div className="bg-[#FCFAFF] p-10 ml-10 rounded-lg border border-[#F5ECFF] w-6/11 h-fit">
-        <Image className="mb-2" width={1024} height={1024} style={{ width: "100%", height: "auto" }} src={`/images/processes/design.png`} alt="" />
+      <div className="w-6/11">
+        {showingSection == "design" && (
+          <div className="bg-[#FCFAFF] ml-10 rounded-lg border border-[#DBBFFF] h-fit relative z-[-1]">
+            <Image
+              className="mb-2"
+              width={1024}
+              height={1024}
+              style={{ width: "100%", height: "auto" }}
+              src={`/images/processes/design.png`}
+              alt=""
+            />
+            <div
+              className="w-10/12 h-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center -z-10"
+              style={{
+                borderRadius: "60rem",
+                background: "radial-gradient(50% 50% at 50% 50%, #E4D0FF 43%, rgba(255, 255, 255, 0.00) 100%)",
+              }}
+            ></div>
+          </div>
+        )}
+        {showingSection == "develop" && (
+          <div className="bg-[#E9F5FF] ml-10 rounded-lg border border-[#85C5FF] h-fit">
+            <Image
+              className="mb-2"
+              width={1024}
+              height={1024}
+              style={{ width: "100%", height: "auto" }}
+              src={`/images/processes/develop.png`}
+              alt=""
+            />
+          </div>
+        )}
+        {showingSection == "empower" && (
+          <div className="bg-[#FFFCF2] ml-10 rounded-lg border border-[#FFE28C] h-fit">
+            <Image
+              className="mb-2"
+              width={1024}
+              height={1024}
+              style={{ width: "100%", height: "auto" }}
+              src={`/images/processes/empower.png`}
+              alt=""
+            />
+          </div>
+        )}
       </div>
     </section>
   );
