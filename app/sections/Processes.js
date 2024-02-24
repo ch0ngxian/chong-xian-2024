39a9a0df417988b33652fcf86c15c7ff95497fec
Chong Xian <chongxian@easystore.co>
2024-02-24 20:14:39 +0800

    add data for processes section

--- app/sections/Processes.js
+++ app/sections/Processes.js
@@ -4,39 +4,54 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Processes() {
-  const [showingSection, setShowingSection] = useState("develop");
+  const sections = [
+    {
+      title: "Design",
+      type: "design",
+      color: "#9747FF",
+      description: "Passionate on drawing clean UI design and provide easy to use experience",
+      tags: ["Figma"],
+    },
+    {
+      title: "Develop",
+      type: "develop",
+      color: "#0085FF",
+      description: "Solve and implement the solution that performance optimised",
+      tags: ["Vue.js", "React", "Laravel", "Ruby on Rails", "MySQL", "AWS"],
+    },
+    {
+      title: "Empower",
+      type: "empower",
+      color: "#E19A0B",
+      description: "Empower the team with the right tools and knowledge",
+      tags: ["Scrum", "Communication", "Leadership"],
+    },
+  ];
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
 
   return (
     <section className="mx-10 my-10 flex justify-between items-center h-screen">
       <div className="flex flex-col justify-center">
         <h1 className="text-xl">In my daily work, I do</h1>
+
         <div className="my-5">
-          <div
-            className={`text-5xl font-bold my-6 ${showingSection == "design" ? "text-[#9747FF]" : "text-gray-200"}`}
-            onClick={() => setShowingSection("design")}
-          >
-            Design
-          </div>
-          <div
-            className={`text-5xl font-bold my-6 ${showingSection == "develop" ? "text-[#0085FF]" : "text-gray-200"}`}
-            onClick={() => setShowingSection("develop")}
-          >
-            Develop
-          </div>
-          <div
-            className={`text-5xl font-bold my-6 ${showingSection == "empower" ? "text-[#E19A0B]" : "text-gray-200"}`}
-            onClick={() => setShowingSection("empower")}
-          >
-            Empower
-          </div>
+          {sections.map((section, index) => (
+            <div
+              key={index}
+              className={`cursor-pointer text-5xl font-bold my-6 ${activeSectionIndex == index ? `text-[${section.color}]` : "text-gray-200"}`}
+              onClick={() => setActiveSectionIndex(index)}
+            >
+              {section.title}
+            </div>
+          ))}
         </div>
-        <div class="text-xl mt-10">Passionate on drawing clean UI design and provide easy to use experience</div>
+        <div className="text-xl mt-10">Passionate on drawing clean UI design and provide easy to use experience</div>
         <div className="mt-5">
           <div className="px-5 py-1 rounded-full bg-gray-200 w-fit">Figma</div>
         </div>
       </div>
       <div className="w-6/11">
-        {showingSection == "design" && (
+        {sections[activeSectionIndex].type == "design" && (
           <div className="bg-[#FCFAFF] ml-10 rounded-lg border border-[#DBBFFF] h-fit relative z-[-1]">
             <Image
               className="mb-2"
@@ -55,7 +70,7 @@ export default function Processes() {
             ></div>
           </div>
         )}
-        {showingSection == "develop" && (
+        {sections[activeSectionIndex].type == "develop" && (
           <div className="bg-[#E9F5FF] ml-10 rounded-lg border border-[#85C5FF] h-fit">
             <Image
               className="mb-2"
@@ -67,7 +82,7 @@ export default function Processes() {
             />
           </div>
         )}
-        {showingSection == "empower" && (
+        {sections[activeSectionIndex].type == "empower" && (
           <div className="bg-[#FFFCF2] ml-10 rounded-lg border border-[#FFE28C] h-fit">
             <Image
               className="mb-2"
