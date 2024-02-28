df46747f77b850923344eb21ea057d8b308c220e
Chong Xian <chongxian@easystore.co>
2024-02-28 21:25:06 +0800

    update thing card for mobile layout

--- app/components/ThingCard.js
+++ app/components/ThingCard.js
@@ -16,9 +16,12 @@ export default function ThingCard({ title, description, url, image_url }) {
         setIsHovered(false);
       }}
     >
-      <div className="absolute left-5 bottom-5 m-auto text-white transition-opacity z-50" style={{ opacity: isHovered ? 1 : 0 }}>
-        <div className="font-medium text-lg ">{title}</div>
-        {description && <div className="font-light">{description}</div>}
+      <div
+        className="absolute left-3 right-3 bottom-3 md:left-5 md:bottom-5 md:right-5 m-auto text-white transition-opacity z-50"
+        style={{ opacity: isHovered ? 1 : 0 }}
+      >
+        <div className="font-medium md:text-lg">{title}</div>
+        {description && <div className="font-light text-sm md:text-base">{description}</div>}
       </div>
 
       <Image key={title} width={512} height={512} style={{ width: "100%", height: "auto" }} src={image_url} alt="" />
