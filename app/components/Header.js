628936448060ec8f0f47aca8d35eb4a0bee6df15
Chong Xian <chongxian@easystore.co>
2024-06-16 12:08:41 +0800

    update header x spacer

--- app/components/Header.js
+++ app/components/Header.js
@@ -20,7 +20,7 @@ export default function Header() {
 
   return (
     <div className="fixed top-0 w-full z-50">
-      <header className="flex justify-between px-7 md:px-11 py-5 rounded-lg backdrop-blur-md w-full bg-white bg-opacity-10">
+      <header className="flex justify-between px-7 py-5 rounded-lg backdrop-blur-md w-full bg-white bg-opacity-10">
         <Image className="cursor-pointer" onClick={() => scrollToTop()} src="/images/logo.svg" alt="Logo" width={60} height={24} priority />
         <div className="hidden sm:flex gap-5">
           {navigations.map((navigation) => (
