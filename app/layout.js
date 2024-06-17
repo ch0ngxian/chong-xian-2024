e7240cb2dab0e82028c1eb5e178266be3c5899eb
Chong Xian <chongxian@easystore.co>
2024-06-17 18:36:54 +0800

    update metadata base

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 export const metadata = {
   title: "Chong Xian Goh - Multi-hyphenate Software Engineer",
   description: "Hi, I am Chong Xian. I design and develop nice product.",
-  metadataBase: new URL("https://acme.com"),
+  metadataBase: new URL("https://www.chongxian.dev"),
   alternates: {
     canonical: "/",
   },
