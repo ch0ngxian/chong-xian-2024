350a484beea262aff3bdddb140899e675d7d3057
Chong Xian <chongxian@easystore.co>
2025-02-09 23:16:47 +0800

    add robots and sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.chongxian.dev",
+      lastModified: "2025-02-09",
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://2023.chongxian.dev",
+      lastModified: "2023-12-31",
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://scrumpoker.chongxian.dev",
+      lastModified: "2023-12-31",
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://draw-a-line.chongxian.dev",
+      lastModified: "2023-12-31",
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
